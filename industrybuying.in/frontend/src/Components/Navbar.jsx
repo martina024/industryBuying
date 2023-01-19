38e6d402184321b5f94ddd3b18f9823aaa24110f
Tarun Sharma <101tash101@gmail.com>
2023-01-19 11:54:26 +0530

    Navbar

--- industrybuying.in/frontend/src/Components/Navbar.jsx
+++ industrybuying.in/frontend/src/Components/Navbar.jsx
@@ -1,10 +1,47 @@
+
+import { Box, Button, Image, Input, Icon, Text } from "@chakra-ui/react";
+import {RxAvatar} from "react-icons/rx"
+import {GoTriangleDown} from "react-icons/go"
+
 import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>
-      
-    </div>
+   <Box>
+    <Box>
+
+    </Box>
+
+
+
+    <Box display="flex" p="30px" h="60px" color="white" alignItems={"center"}  justifyContent="space-between" bg="#232f3e">
+    
+    <Box>
+    <Image src="https://www.industrybuying.com/static/images/industry-buying-white-logo.png"/>
+    </Box>
+    
+    <Box display="flex" >
+    <Button px="30px" bg="#e9611e" >All Categories</Button>
+    <Input bg="white" w="450px" placeholder='Search Products by title, supplier, sku, category, brand, etc...' />
+    </Box>
+    
+    <Box display="flex" >
+    <Icon m="10px"  boxSize={"30px"}  as={RxAvatar}/>
+   
+    <Box>
+      <Box display="flex">
+        <Text>
+          Sign In
+        </Text>
+        <Icon ml="25px" mt="5px" as={GoTriangleDown}/>
+      </Box>
+      <Text>
+        My Account
+      </Text>
+    </Box>
+    </Box>
+    </Box>
+   </Box>
   )
 }
 
--- industrybuying.in/frontend/src/Pages/HomePage.jsx
+++ industrybuying.in/frontend/src/Pages/HomePage.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Navbar from '../Components/Navbar'
+
 
 const HomePage = () => {
   return (
     <div>
-      
+      <Navbar/>
     </div>
   )
 }
