e947cf87832eafee41adc400b5b8d6ac4c3787ba
Randeep Singh <randeepsingh986@gmail.com>
2023-01-20 17:46:15 +0530

    login sigup UI

--- industrybuying.in/frontend/src/Pages/Admin/AdminLogin.jsx
+++ industrybuying.in/frontend/src/Pages/Admin/AdminLogin.jsx
@@ -1,11 +1,59 @@
-import React from 'react'
+import React from 'react';
+import {
+  Flex,
+  Heading,
+  Input,
+  Button,
+  FormControl,
+  FormLabel,
+  Switch,
+  useColorMode,
+  useColorModeValue,
+} from '@chakra-ui/react';
 
-const AdminLogin = () => {
+const Login = () => {
+  const { toggleColorMode } = useColorMode();
+  const formBackground = useColorModeValue('gray.100', 'gray.700');
+
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <Flex h="100vh" alignItems="center" justifyContent="center">
+      <Flex
+        flexDirection="column"
+        bg={formBackground}
+        p={12}
+        borderRadius={8}
+        boxShadow="lg"
+      >
+        <Heading mb={6}>Log In</Heading>
+        <Input
+          placeholder="john@gmail.com"
+          type="email"
+          variant="filled"
+          mb={3}
+        />
+        <Input
+          placeholder="**********"
+          type="password"
+          variant="filled"
+          mb={6}
+        />
+        <Button colorScheme="teal" mb={8}>
+          Log In
+        </Button>
+        <FormControl display="flex" alignItems="center">
+          <FormLabel htmlFor="dark_mode" mb="0">
+            Enable Dark Mode?
+          </FormLabel>
+          <Switch
+            id="dark_mode"
+            colorScheme="teal"
+            size="lg"
+            onChange={toggleColorMode}
+          />
+        </FormControl>
+      </Flex>
+    </Flex>
+  );
+};
 
-export default AdminLogin
+export default Login;
--- industrybuying.in/frontend/src/Pages/User/UserLogin.jsx
+++ industrybuying.in/frontend/src/Pages/User/UserLogin.jsx
@@ -1,11 +1,129 @@
-import React from 'react'
 
-const UserLogin = () => {
-  return (
-    <div>
+
+import {
+  Button,
+  Checkbox,
+  Flex,
+  FormControl,
+  FormLabel,
+  Heading,
+  Input,
+  Link,
+  Stack,
+  Image,
+  useColorModeValue,
+  useColorMode,
+  Switch,
+  useToast,
+} from '@chakra-ui/react';
+import axios from 'axios';
+import { useState } from 'react';
+import image from '../User/logo.jpg'
+export default function UserLogin() {
+  const toast=useToast()
+  const { toggleColorMode } = useColorMode();
+  const formBackground = useColorModeValue('gray.100', 'gray.700');
+ 
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleSubmit = () => {
+    const payload = {
       
-    </div>
-  )
-}
+      email,
+      password,
+      
+    };
+   
+    if(!payload.email){
 
-export default UserLogin
+      toast({
+        title: "Please fill your Email",
+        description: "Your email is missing",
+        status: "error",
+        duration: 3000,
+        isClosable: true,
+      });
+      return
+    }
+    else if(!payload.password){
+
+      toast({
+        title: "Please fill your Password",
+        description: "Your password is missing",
+        status: "error",
+        duration: 3000,
+        isClosable: true,
+      });
+      return
+    }
+    
+    console.log(payload);
+    //if successfull
+    {
+
+      toast({
+      title: "Your are successfully logged in",
+      status: "success",
+      duration: 4000,
+      isClosable: true,
+    });
+    return axios
+      .post("", payload)
+      .then((res) => console.log(res.data));
+      
+    }
+  };
+  return (
+    <Stack minH={'100vh'} direction={{ base: 'column', md: 'row' }}>
+   
+        <Flex h="100vh" alignItems="center" justifyContent="center" w="60%">
+      <Flex
+        flexDirection="column"
+        bg={formBackground}
+        p={12}
+        borderRadius={8}
+        boxShadow="lg"
+      >
+        <Heading mb={6}>User Log In</Heading>
+        <Input
+          placeholder="john@gmail.com"
+          type="email"
+          variant="filled"
+          mb={3}
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          placeholder="**********"
+          type="password"
+          variant="filled"
+          mb={6}
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <Button colorScheme="teal" mb={8}  onClick={handleSubmit}>
+          Log In
+        </Button>
+        <FormControl display="flex" alignItems="center">
+          <FormLabel htmlFor="dark_mode" mb="0">
+            Enable Dark Mode?
+          </FormLabel>
+          <Switch
+            id="dark_mode"
+            colorScheme="teal"
+            size="lg"
+            onChange={toggleColorMode}
+          />
+        </FormControl>
+      </Flex>
+    </Flex>
+      <Flex flex={1}>
+        <Image h="90vh"
+          alt={'Login Image'}
+          objectFit={'cover'}
+          src={image}
+        />
+      </Flex>
+    </Stack>
+  );
+}
