e6893dae60471e02ebcf7e72b9e6e6d918ae6819
Martina Varghese <martyrabby024@gmail.com>
2023-01-23 06:20:28 +0530

    login signup

---
+++ src/Components/Admin/AdminNavbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const AdminNavbar = () => {
+  return (
+    <div>AdminNavbar</div>
+  )
+}
+
+export default AdminNavbar
+
+//  fields in navbar to mention here-:
+// 1. DashBoard
+// 2. All Products
+// 3.logout/login
---
+++ src/Components/Product.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { Button } from "../styles/Button";
+const Product = (curElem) => {
+  const { _id, title,brand, images, price, category } = curElem;
+  return (
+    <NavLink to={`/product/${_id}`}>
+      <div className="card">
+        <figure>
+          <img src={images[0].image_url} alt={title} />
+          <figcaption className="caption">{category.slice(0, 10)}</figcaption>
+        </figure>
+
+        <div className="card-data">
+          <div className="card-data-flex">
+            <h3>{brand}</h3>
+            <p className="card-data--price">{price}</p>
+          </div>
+          <p>{title.slice(0,40)}</p>
+        </div>
+        <div className='cart-data-button'>
+        <Button>Add to Cart</Button>
+        <Button className='cart-data-buy'>Buy now</Button>
+        </div>
+      </div>
+    </NavLink>
+  );
+};
+
+export default Product;
